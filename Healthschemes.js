3f8a3065927645418ad56310325cf7d483d90a91
Boomitha-B <poojaunity2006@gmail.com>
2024-10-05 04:42:40 +0530

    Policy Category Intent

--- Healthschemes.js
+++ Healthschemes.js
@@ -144,3 +144,38 @@
     "languageCode": "en"
   }
 }
+
+
+{
+  "responseId": "b254cd9c-313e-472b-9ccc-d6e843e06de7-d673503e",
+  "queryResult": {
+    "queryText": "adults",
+    "parameters": {
+      "geo-state": "",
+      "Get_Policy_Age": "30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65",
+      "Get_Policy_Disease": "cancer",
+      "Get_Policy_Category": "adults"
+    },
+    "allRequiredParamsPresent": true,
+    "fulfillmentText": "Alright!! I can guide you with the health policies",
+    "fulfillmentMessages": [
+      {
+        "text": {
+          "text": [
+            "Alright!! I can guide you with the health policies"
+          ]
+        }
+      }
+    ],
+    "intent": {
+      "name": "projects/planfinder-niad/agent/intents/ba50d1da-403a-46b0-86f6-b0d7d573c1a7",
+      "displayName": "schemes",
+      "endInteraction": true
+    },
+    "intentDetectionConfidence": 1,
+    "diagnosticInfo": {
+      "end_conversation": true
+    },
+    "languageCode": "en"
+  }
+}
