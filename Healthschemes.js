bdbb48c02f5776175d22c0e4a91a728708365c08
Boomitha-B <poojaunity2006@gmail.com>
2024-10-04 16:29:32 +0530

    Age, Disease Intent

--- Healthschemes.js
+++ Healthschemes.js
@@ -63,3 +63,84 @@
     "languageCode": "en"
   }
 }
+
+
+{
+  "responseId": "f4225d6d-89a4-42fd-901f-c4b09fc537c7-d673503e",
+  "queryResult": {
+    "queryText": "children",
+    "parameters": {
+      "geo-state": "",
+      "Get_Policy_Disease": "",
+      "Get_Policy_Age": "Children"
+    },
+    "fulfillmentText": "What is the Get_Policy_Disease?",
+    "fulfillmentMessages": [
+      {
+        "text": {
+          "text": [
+            "What is the Get_Policy_Disease?"
+          ]
+        }
+      }
+    ],
+    "outputContexts": [
+      {
+        "name": "projects/planfinder-niad/agent/sessions/22f9c117-6d4e-422e-c94c-852d5d97adbe/contexts/ba50d1da-403a-46b0-86f6-b0d7d573c1a7_id_dialog_context",
+        "lifespanCount": 2,
+        "parameters": {
+          "Get_Policy_Age": "Children",
+          "geo-state": "",
+          "Get_Policy_Disease.original": "",
+          "geo-state.original": "",
+          "Get_Policy_Disease": "",
+          "Get_Policy_Age.original": "children"
+        }
+      },
+      {
+        "name": "projects/planfinder-niad/agent/sessions/22f9c117-6d4e-422e-c94c-852d5d97adbe/contexts/schemes_dialog_context",
+        "lifespanCount": 2,
+        "parameters": {
+          "geo-state": "",
+          "Get_Policy_Disease.original": "",
+          "Get_Policy_Age.original": "children",
+          "Get_Policy_Age": "Children",
+          "geo-state.original": "",
+          "Get_Policy_Disease": ""
+        }
+      },
+      {
+        "name": "projects/planfinder-niad/agent/sessions/22f9c117-6d4e-422e-c94c-852d5d97adbe/contexts/schemes_dialog_params_get_policy_disease",
+        "lifespanCount": 1,
+        "parameters": {
+          "Get_Policy_Disease": "",
+          "Get_Policy_Age.original": "children",
+          "geo-state": "",
+          "Get_Policy_Age": "Children",
+          "Get_Policy_Disease.original": "",
+          "geo-state.original": ""
+        }
+      },
+      {
+        "name": "projects/planfinder-niad/agent/sessions/22f9c117-6d4e-422e-c94c-852d5d97adbe/contexts/_system_counters_",
+        "lifespanCount": 1,
+        "parameters": {
+          "no-input": 0,
+          "no-match": 0,
+          "geo-state.original": "",
+          "geo-state": "",
+          "Get_Policy_Age": "Children",
+          "Get_Policy_Age.original": "children",
+          "Get_Policy_Disease": "",
+          "Get_Policy_Disease.original": ""
+        }
+      }
+    ],
+    "intent": {
+      "name": "projects/planfinder-niad/agent/intents/ba50d1da-403a-46b0-86f6-b0d7d573c1a7",
+      "displayName": "schemes"
+    },
+    "intentDetectionConfidence": 1,
+    "languageCode": "en"
+  }
+}
