a7dd2aebaf151fd2d106ec8d1f122dffc0348100
Boomitha-B <poojaunity2006@gmail.com>
2024-10-04 16:18:24 +0530

    Welcome Indent

---
+++ Healthschemes.js
@@ -0,0 +1,26 @@
+{
+    "responseId": "384360dc-c8aa-4298-b151-a098f0fd6200-d673503e",
+    "queryResult": {
+      "queryText": "planfinder",
+      "action": "input.welcome",
+      "parameters": {},
+      "allRequiredParamsPresent": true,
+      "fulfillmentText": "Hello!! I am your PlanFinder. How can I assist you?",
+      "fulfillmentMessages": [
+        {
+          "text": {
+            "text": [
+              "Hello!! I am your PlanFinder. How can I assist you?"
+            ]
+          }
+        }
+      ],
+      "intent": {
+        "name": "projects/planfinder-niad/agent/intents/3074d33d-83e7-450e-af9a-17625458ad86",
+        "displayName": "Default Welcome Intent"
+      },
+      "intentDetectionConfidence": 0.70064396,
+      "languageCode": "en"
+    }
+  }
+  
